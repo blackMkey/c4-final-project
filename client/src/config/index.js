d363687520b047603960ab67b73c5e47e971deb6
quocdung.phi <quocdung.phi@gmail.com>
2023-05-17 09:40:08 +0700

    update dockerFile

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,7 @@
+const url = "http://aaeba85624d5946f883819895fde5fd6-150654271.us-east-1.elb.amazonaws.com"
 export const config = {
     apiId: '7fnf1wopb0',
     clientID: 'j5mOey9tDJ08X9WKrZTnc4XgX4xnoWE8',
     domain: 'phiquocdung-dev.us.auth0.com',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: url+'/callback'
 }
